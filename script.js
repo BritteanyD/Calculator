c07aee8c014aa8b467316c61c33c814ca70b51d1
Britteany <miszbritteany@gmail.com>
2023-09-28 19:45:30 -0400

    Created functions to test the operatiors

---
+++ script.js
@@ -0,0 +1,20 @@
+//add tests
+function add(a, b) {
+    return a + b;
+};
+console.log("1+7=8", add(1, 7) === 8)
+//subtract tests
+function subtract(a, b){
+    return a - b;
+}
+console.log("100-200=-100", subtract(100, 200) === -100)
+//multiply tests
+function multiply(a, b){
+    return a * b;
+}
+console.log("2 * 3= 6", multiply(2, 3) === 6)
+//divide tests
+function divide(a, b) {
+    return a / b;
+}
+console.log("10/5=2", divide(10, 5) === 2)
